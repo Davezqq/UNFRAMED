37467954b49d32193a3138acaf6ef667d693b9e7
Davezqq <136834413@qq.com>
2025-04-14 00:11:42 -0400

    update

--- src/vocabulary.py
+++ src/vocabulary.py
@@ -4,6 +4,7 @@ import os
 from collections import defaultdict 
 from tqdm import tqdm 
 from rdkit import Chem, DataStructs
+import argparse
 
 def smiles2mol(smiles):
 	mol = Chem.MolFromSmiles(smiles)
@@ -63,42 +64,42 @@ def smiles2word(smiles):
 
 
 
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--data_path','-d',type=str)
+	parser.add_argument('--threshold', '-th', type=int)
+	args = parser.parse_args()
 
+	data_path = args.data_path
+	parent_dir = list(data_path.split('/')[:-1])
+	all_vocabulary_file = '/'.join(parent_dir+["all_structures.txt"])
+	select_vocabulary_file = '/'.join(parent_dir+["vocabulary.txt"])
+	min_show_time = args.threshold
 
-all_vocabulary_file = "data/train_set/Molecule_dataset.txt"
-rawdata_file = "data/train_set/all_structures.txt"
-select_vocabulary_file = "data/vocabulary.txt"
-min_show_time = 1000
+	if not os.path.exists(all_vocabulary_file):
+		with open(data_path) as fin:
+			lines = fin.readlines()[1:]
+			smiles_lst = [i.strip('\n').strip('\r').split(' ')[0] for i in lines]
+		word2cnt = defaultdict(int)
+		for smiles in tqdm(smiles_lst):
+			word_lst = smiles2word(smiles)
+			if word_lst is None:
+				continue
+			for word in word_lst:
+				word2cnt[word] += 1
+		word_cnt_lst = [(word,cnt) for word,cnt in word2cnt.items()]
+		word_cnt_lst = sorted(word_cnt_lst, key=lambda x:x[1], reverse = True)
 
-if not os.path.exists(all_vocabulary_file):
-	with open(rawdata_file) as fin:
-		lines = fin.readlines()[1:]
-		smiles_lst = [i.strip('\n').strip('\r').split(' ')[0] for i in lines]
-	word2cnt = defaultdict(int)
-	for smiles in tqdm(smiles_lst):
-		word_lst = smiles2word(smiles)
-		if word_lst is None:
-			continue
-		for word in word_lst:
-			word2cnt[word] += 1
-	word_cnt_lst = [(word,cnt) for word,cnt in word2cnt.items()]
-	word_cnt_lst = sorted(word_cnt_lst, key=lambda x:x[1], reverse = True)
+		with open(all_vocabulary_file, 'w') as fout:
+			for word, cnt in word_cnt_lst:
+				fout.write(word + '\t' + str(cnt) + '\n')
+	else:
+		with open(data_path, 'r') as fin:
+			lines = fin.readlines()
+			word_cnt_lst = [(line.split('\t')[0], int(line.split('\t')[1])) for line in lines]
 
-	with open(all_vocabulary_file, 'w') as fout:
+	word_cnt_lst = list(filter(lambda x:x[1]>min_show_time, word_cnt_lst))
+
+	with open(select_vocabulary_file, 'w') as fout:
 		for word, cnt in word_cnt_lst:
 			fout.write(word + '\t' + str(cnt) + '\n')
-else:
-	with open(all_vocabulary_file, 'r') as fin:
-		lines = fin.readlines()
-		word_cnt_lst = [(line.split('\t')[0], int(line.split('\t')[1])) for line in lines]
-
-
-word_cnt_lst = list(filter(lambda x:x[1]>min_show_time, word_cnt_lst))
-print(len(word_cnt_lst))
-
-with open(select_vocabulary_file, 'w') as fout:
-	for word, cnt in word_cnt_lst:
-		fout.write(word + '\t' + str(cnt) + '\n')
-
-
-
